70b4374dd6e8bc69bf26b093ec1a4f3132a76840
kwansu <kwansu91@naver.com>
2021-06-26 22:30:26 +0900

    sks

--- LogisticRegression_basic.py
+++ LogisticRegression_basic.py
@@ -1,22 +1,61 @@
-from NumericalDifferentiation import numerical_derivative
-import numpy as np
-import math
+from NumericalDifferentiation import*
 
-x_data = [60, 73, 90, 55, 81, 70, 69, 95, 88, 83, 65]
+x_data = np.array([60, 73, 90, 55, 81, 70, 69, 95, 88, 83, 65])
 y_data = [False, False, True, False, True,
-          False, False, False, True, True, True, False]
+          False, False, True, True, True, False]
 
-y_data = [(1 if element else 0) for element in y_data]
-
-def hypothesisFuction(x,w,b):
-    return np.dot(x,w) + b
-
-
-def sigmoidFunction(f):
-    return 1/(1-math.exp(f))
+x_data_normalization = (x_data - np.mean(x_data, axis=0)
+                        ) / np.std(x_data, axis=0)
+x_data_normalization = np.reshape(
+    x_data_normalization, [len(x_data_normalization), 1])
+y_data = np.array([(1. if element else 0.) for element in y_data])
+y_data = np.reshape(y_data, [len(y_data), 1])
 
 
-def costFunction(x,w,b):
-   pass 
+def hypothesisFunction(x, w, b):
+    g = np.dot(x, w) + b
+    temp = sigmoidFunction(g)
+    return temp
 
-print(y_data)
+
+def costFunction(x, w, b):
+    h = hypothesisFunction(x, w, b)
+    delta = 1e-7
+    temp = y_data*np.log(h+delta) + (1-y_data)*np.log(1-h+delta)
+    return -np.sum(temp)
+
+
+W = np.array([1.])
+W = np.reshape(W, [1, 1])
+b = np.array([1.])
+
+
+def predict(x):
+    y = hypothesisFunction(x, W, b)
+    iterator = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    _x = np.reshape(x_data, [11, 1])
+    sameCount = 0
+
+    while not iterator.finished:
+        iterIndex = iterator.multi_index
+        predic_value = True if y[iterIndex] >= 0.5 else False
+        print("x : {} , predict : {}".format(_x[iterIndex], predic_value))
+        if predic_value == bool(y_data[iterIndex]):
+            sameCount+=1
+        iterator.iternext()
+
+    print("accuracy : %f" % (sameCount / y_data.size))
+
+
+for i in range(1000):
+    print('epoch %d, cost : %f' %
+          (i, costFunction(x_data_normalization, W, b)))
+    t1 = numerical_derivative(
+        lambda t: costFunction(x_data_normalization, t, b), W)
+    W -= 0.01 * t1
+    b -= 0.01 * \
+        numerical_derivative(lambda t: costFunction(
+            x_data_normalization, W, t), b)
+
+print("W : {}, b : {}".format(W, b))
+predict(x_data_normalization)
--- NumericalDifferentiation.py
+++ NumericalDifferentiation.py
@@ -1,19 +1,30 @@
 import numpy as np
+import math
 
 def numerical_derivative(expression, x):
-    limitDistance = 0.0001 #1e-4
     result = np.zeros_like(x)
     iterator = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
 
     while not iterator.finished:
         iterIndex = iterator.multi_index
         tempCopy = x[iterIndex]
-        x[iterIndex] = tempCopy + limitDistance
+        limitDistance = 1e-4 #* tempCopy
+
+        x[iterIndex] = float(tempCopy) + limitDistance
         plusDx = expression(x)
-        x[iterIndex] = tempCopy - limitDistance
+
+        x[iterIndex] = float(tempCopy) - limitDistance
         minusDx = expression(x)
+
         result[iterIndex] = (plusDx - minusDx) / (2 * limitDistance)
         x[iterIndex] = tempCopy
         iterator.iternext()
         
     return result
+
+
+def sigmoidFunction(f):
+    # temp2 = math.e ** (-f)
+    # temp = 1/(1+temp2)
+    # return temp
+    return 1 / (1+np.exp(-f))
